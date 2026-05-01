ff1805dbae744f49113ec2da9ce7c6d50b67dbb6
werner2011 <maksymilian.korbik@student.put.poznan.pl>
2026-05-01 21:48:37 +0200

    Dodano sekwenator

--- Sekwenator/MainWindow.axaml.cs
+++ Sekwenator/MainWindow.axaml.cs
@@ -1,34 +1,37 @@
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using System.Collections.Generic;
+using Avalonia.Controls; //daje dostep do elementow okna 
+using Avalonia.Interactivity; 
+using System.Collections.Generic; //daje dostep do obslugi klikniecia przycisku 
 using System.Text;
 
 namespace Sekwenator
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() //MainWindows jest oknem aplikacji 
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
 
-        private void CountButton_Click(object? sender, RoutedEventArgs e)
+        private void CountButton_Click(object? sender, RoutedEventArgs e) //metoda klikniecia przycisku 
         {
-            TextBox inputTextBox = this.FindControl<TextBox>("InputTextBox");
+            //pobranie kontrolek z okna: 
+            TextBox inputTextBox = this.FindControl<TextBox>("InputTextBox"); 
             TextBlock resultTextBlock = this.FindControl<TextBlock>("ResultTextBlock");
 
-            string dna = inputTextBox.Text;
-            dna = dna.ToUpper();
-
+            string dna = inputTextBox.Text; //pobranie sekwencji z wejœcia 
+            dna = dna.ToUpper(); //zamiana na duze litery 
+            //Slownik klucz-wartosc na zliczanie fragmentow 
+            //kluczem jest tekst, a waroscia liczba calkowita zgodnie z <string,int> 
             Dictionary<string, int> counts = new Dictionary<string, int>();
 
+            //petla for po sekwencji 
             for (int i = 0; i <= dna.Length - 4; i++)
             {
-                string fragment = dna.Substring(i, 4);
+                string fragment = dna.Substring(i, 4); //wycinanie 4-nukleotydowego fragmentu z sekwencji, za pomoc¹ Substring 
 
-                if (counts.ContainsKey(fragment))
+                if (counts.ContainsKey(fragment)) //sprawdzenie czy fragment siê ju¿ pojawil? 
                 {
-                    counts[fragment] = counts[fragment] + 1;
+                    counts[fragment] = counts[fragment] + 1; //zliczenie fragmentu, jesli pojawil siê wczesniej
                 }
                 else
                 {
@@ -38,12 +41,12 @@ namespace Sekwenator
 
             StringBuilder result = new StringBuilder();
 
-            foreach (var item in counts)
+            foreach (var item in counts) //petla foreach po elementach slownika i wypisanie wyników 
             {
                 result.AppendLine(item.Key + " : " + item.Value);
             }
 
-            resultTextBlock.Text = result.ToString();
+            resultTextBlock.Text = result.ToString(); //zamiana na tekst 
         }
     }
 }
